92b0e128463a7bc66c63541fa9f74b3017a6fcb3
RakhKaran <132744751+RakhKaran@users.noreply.github.com>
2026-01-13 22:10:41 +0530

    template changes done

--- src/templates/registration-welcome.template.ts
+++ src/templates/registration-welcome.template.ts
@@ -3,7 +3,7 @@ export default function generateWelcomeTemplate(mailOptions: any) {
     <!DOCTYPE html>
     <html>
     <head>
-      <title>Welcome to Altiv.AI– Take Charge of Your AI Future</title>
+      <title>Welcome to Altiv</title>
       <style>
         body { font-family: Arial, sans-serif; }
         .box {
@@ -21,41 +21,6 @@ export default function generateWelcomeTemplate(mailOptions: any) {
     <body>
       <div class="box">
         <p>Hi ${mailOptions.firstName},</p>
-
-        <p>
-          Welcome to Altiv.AI—your trusted partner in navigating the AI revolution.
-          With AI transforming industries faster than ever, it’s time to future-proof your career with actionable, personalized insights.
-        </p>
-
-        <h3>Start with FOBO – Your Personalized AI Impact Report</h3>
-
-        <p>Unlike generic tools that base results on job titles or quick surveys, FOBO (Fear of Becoming Obsolete) uses state-of-the-art AI to analyze what you actually do. It identifies:</p>
-
-        <ul>
-          <li>Tasks that can be AI-Automated.</li>
-          <li>Tasks that can be AI-Augmented.</li>
-          <li>Human-Only tasks that remain uniquely yours.</li>
-        </ul>
-
-        <p>This customized analysis gives you clarity on how AI will affect your role and provides a tailored roadmap to stay ahead.</p>
-
-         <p class="cta">
-          👉 <a href="${mailOptions.foboUrl}" target="_blank">Check FOBO Score</a>
-        </p>
-
-
-
-        <p>
-          Explore how Altiv.AI can help you stay ahead of the AI curve with expert-curated blogs, role-specific courses, and a community of forward-thinking professionals.
-        </p>
-
-        <p>The AI revolution is here. Take charge of your future today.</p>
-
-       <p class="cta">
-          👉 <a href="${mailOptions.foboUrl}" target="_blank">Start Your FOBO Scan</a>
-        </p>
-
-        <p>Here’s to staying ahead,<br/>The Altiv.AI Team</p>
       </div>
     </body>
     </html>
